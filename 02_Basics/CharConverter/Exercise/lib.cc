988d556d77c810592be5459b49e4c44ac6f27069
Niklas Guntelmann <niklasguntelmann@live.de>
2024-11-04 19:28:11 +0100

    Basics Fertig

--- 02_Basics/CharConverter/Exercise/lib.cc
+++ 02_Basics/CharConverter/Exercise/lib.cc
@@ -1,29 +1,38 @@
 #include "lib.h"
+#include <cstdint>
+constexpr auto k_conversion_factor = std::uint8_t{32};
 
 bool is_numeric(const char character)
 {
+    return (character > '0' && character < '9');
 }
-
-bool is_alpha(const char character)
-{
-}
-
-bool is_alpha_numeric(const char character)
-{
-}
-
 bool is_upper_case(const char character)
 {
+    return (character > 'A' && character < 'Z');
 }
 
 bool is_lower_case(const char character)
 {
+    return (character > 'a' && character < 'z');
 }
+bool is_alpha(const char character)
+{
+    return (is_upper_case(character) || is_lower_case(character));
+}
+
+bool is_alpha_numeric(const char character)
+{
+    return(is_alpha(character) || is_numeric(character));
+}
+
+
 
 char to_upper_case(const char character)
 {
+    return character - k_conversion_factor;
 }
 
 char to_lower_case(const char character)
 {
+    return  character + k_conversion_factor;
 }
--- 02_Basics/IntCalculator/Exercise/lib.cc
+++ 02_Basics/IntCalculator/Exercise/lib.cc
@@ -4,12 +4,40 @@
 
 std::uint32_t modulo(const std::uint32_t number_a, const std::uint32_t number_b)
 {
+    uint32_t ganzzahliger_quotient = 0;
+    uint32_t rest = 0;
+
+    ganzzahliger_quotient = number_a / number_b;
+    rest = number_a - (ganzzahliger_quotient * number_b);
+
+    return rest;
 }
 
 std::uint32_t sum_of_digits(std::uint32_t number)
 {
+    std::uint32_t num_digits = 0;
+
+    do
+    {
+        number /= 10;
+        num_digits++;
+    } while (number > 0);
+
+    return num_digits;
 }
 
 std::uint32_t cross_sum(std::uint32_t number)
 {
+    std::uint32_t sum = 0;
+
+    while (number > 0)
+    {
+        std::uint32_t current_digit = number % 10;
+
+        sum += current_digit;
+
+        number /= 10;
+    }
+
+    return sum;
 }
